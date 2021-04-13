5c3f8fa605bbcbc7caa1bb7b81ff4fd7f28c6ff3
Simonbiker <smacr25@gmail.com>
2021-04-13 22:47:06 +0100

    Adding code to ts file to change the bankground color when clicked button. This will not remain. The button should change the container color which will happen later.

--- src/app/front-end-projects/color-change/color-change.component.ts
+++ src/app/front-end-projects/color-change/color-change.component.ts
@@ -10,6 +10,19 @@ export class ColorChangeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+    let btn = document.getElementById('btn');
+    let color = document.querySelector('.color');  
+
+    btn.addEventListener('click', function(){
+      // a var to hold the #
+      let hexColor = '#';
+      for(let i = 0; i<6; i++){
+          hexColor += hex[Math.floor(Math.random() * hex.length)];
+      }
+     color.textContent = hexColor; 
+    document.body.style.backgroundColor = hexColor;
+  });
   }
 
 }
